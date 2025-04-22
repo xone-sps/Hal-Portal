9d2c7410e66be3e8bea694d5718a912df52dacdf
Xone <xone.sps@gmail.com>
2025-04-22 09:44:54 +0700

    fix api url

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, 'src'), // ✅ absolute path
     },
   },
   envPrefix: 'VITE_', // ✅ Make sure VITE_ prefix is set
